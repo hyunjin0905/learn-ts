0be19268b99817321f56a0da190a7bf8f40b75cd
조현진 <kkadangj@gmail.com>
2021-01-27 22:52:05 +0900

    no message

---
+++ class-note/10_type-assertion.ts
@@ -0,0 +1,7 @@
+// 타입 단언 (type assertion)
+var a;
+var b = a as string;
+
+// DOM API 조작
+var div = document.querySelector('div');
+ 
--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,12 +7,42 @@ var a = 'abc';
  }
 
  // 타입추론 기본 2
+// interface Dropdown<T> {
+//      value: T;
+//      title: string;
+// }
+//
+// var shoppingItem: Dropdown<number> = {
+//      value:1,
+//      title: 'hi'
+// }
+
+
+// 타입추론 기본 3
 interface Dropdown<T> {
-     value: T;
-     title: string;
+    value: T;
+    title: string;
 }
 
-var shoppingItem: Dropdown<number> = {
-     value:1,
-     title: 'hi'
+
+interface DetailedDropDown<K>  extends Dropdown<K>{
+    name: K;
+    id: number;
 }
+
+var shoppingItem: DetailedDropDown<number> = {
+    value: 1,
+    title: 'hi',
+    name: 1,
+    id: 1
+
+}
+
+
+
+
+// Best Common Type  가장 근접한 타입을 추론한다  union 으로 묶어나간다!
+var arr = [1,2,true] // (number \ boolean)[]
+
+
+//
